34c5753865664e2ddaa8aa6e35bd91b187829d62
Leandro180421! <leonardoleeko88@gmail.com>
2025-01-15 16:45:41 -0300

    configurado o .env para envio de email pelo programa Mailhug, configurado uma rota para teste de envio de e-mail.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     echo "Bem vindo";
+});
+
+Route::get('/mail', function () {
+    Mail::raw('Mensagem de teste de RH MANGNT', function (Message $message) {
+        $message->to('teste@email.com')
+            ->subject('Bem vindo ao RH MANGNT')
+            ->from('rh@email.com');
+    });
+
+    echo 'Email enviado com sucesso';
 });
